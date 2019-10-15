80a584e32c58958c1952868b8198445fb9dfc414
William Johansson <radar@radhuset.org>
2019-10-15 21:38:03 +0200

    Add Reporter multiplexer

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,40 @@
 package handler
 
+import "sync"
+
 type Reporter interface {
 	ReportPower(on bool)
 	ReportVolume(volume int)
+}
+
+type multiplexer []Reporter
+
+func Multiplex(reporters ...Reporter) Reporter {
+	return (multiplexer)(reporters)
+}
+
+func (mux multiplexer) fanOut(inner func(Reporter)) {
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	wg.Add(len(mux))
+	for _, muxReporter := range mux {
+		reporter := muxReporter
+		go func() {
+			defer wg.Done()
+			inner(reporter)
+		}()
+	}
+}
+
+func (mux multiplexer) ReportPower(on bool) {
+	mux.fanOut(func(reporter Reporter) {
+		reporter.ReportPower(on)
+	})
+}
+
+func (mux multiplexer) ReportVolume(volume int) {
+	mux.fanOut(func(reporter Reporter) {
+		reporter.ReportVolume(volume)
+	})
 }
