48b4447e0b60fded015c67ef4708d4346cfc5643
William Johansson <radar@radhuset.org>
2019-10-15 22:06:32 +0200

    Run MQTT reporter

--- main.go
+++ main.go
@@ -5,20 +5,29 @@ import (
 	"os"
 	"time"
 
+	"github.com/radhus/vsxknob/handler"
+	"github.com/radhus/vsxknob/mqtt"
 	"github.com/radhus/vsxknob/prometheus"
 	"github.com/radhus/vsxknob/vsx"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalln("Usage: vsx-addr:8102")
+	if len(os.Args) != 3 {
+		log.Fatalln("Usage: vsx-addr:8102 mqtt-addr:1883")
 	}
 	addr := os.Args[1]
+	mqttAddr := os.Args[2]
 
 	prom := prometheus.New(addr)
 	go prom.Start(":8080")
 
-	connection, err := vsx.Connect(addr, prom)
+	mqtt, err := mqtt.New(mqttAddr)
+	if err != nil {
+		log.Fatalln("MQTT failed: ", err)
+	}
+
+	reporterMux := handler.Multiplex(prom, mqtt)
+	connection, err := vsx.Connect(addr, reporterMux)
 	if err != nil {
 		log.Fatalln("Couldn't connect to VSX:", err)
 	}
